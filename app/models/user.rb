5e6ea01927a80e7c724da744db20194c2076b0af
Todd M <tboogie87@me.com>
2018-08-09 11:04:36 -0400

    added user path

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    def self.get_country_api(cannabis_lineage)
+        response = HTTParty.get('{API_key}' + location)
+        return parsed_response = JSON.parse(response.body)  
 end
