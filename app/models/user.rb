80c30d601243f5e45ae913d53bab93ed1dc4da7c
Todd M <tboogie87@me.com>
2018-08-09 16:52:24 -0400

    added API Key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    def self.get_country_api(cannabis_lineage)
-        response = HTTParty.get('{API_key}' + location)
+    def self.get_country_api(cannabis_location)
+        response = HTTParty.get('pk.eyJ1IjoidG9kZG04NyIsImEiOiJjamtteW5hODYwZ2VyM3FxcjhxdHY1M3NoIn0.txU0n14hIpXGRxUPC56jng' + location)
         return parsed_response = JSON.parse(response.body)  
 end
