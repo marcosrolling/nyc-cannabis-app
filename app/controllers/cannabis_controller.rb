8a6f768a7e54398c9a6ac0643675b471bf610422
Todd M <tboogie87@me.com>
2018-08-10 11:49:30 -0400

    added :edit, :update, :create

--- app/controllers/cannabis_controller.rb
+++ app/controllers/cannabis_controller.rb
@@ -1,37 +1,45 @@
 class CannabisController < ApplicationController
 
-    def new
-        @cannabi = Cannabi.new
-    end
-
     def index 
-        @cannabis = Cannabi.all.order(name: :asc)
-        if params['cannabi']
-            @cannabis = Cannabi.all.order(params['cannabi']['strain'])
-        end
-    end
-
-    def sort_cannabis
-        binding.pry
-    end
-
-    def create
-        @cannabis = Cannabis.new
-        # cannabis_data = Cannabis.get_cannabis_api(ucpc)
-        @cannabi.strain = cannabi_info['strain']
-        @cannabi.flag_url = cannabi_info['flag_url']
-        @cannabi.img_url = cannabi_info['img_url']
-        @cannabi.origin = cannabi_info['origin']
-        @cannabi.data = cannabi_info['data']
-        @cannabi.save
+        @cannabis = Cannabi.all
+        render :index
     end
 
     def show
         @cannabi = Cannabi.find(params[:id])
     end
 
+    def new
+        @cannabi = Cannabi.new
+        render :new
+    end
+
+    def create
+        @cannabi = Cannabi
+        cannabi_params = params.require(:cannabi).permit(:strain, :img_url, :origin, :flag_url, :data, :lat, :lon)
+        cannabi = Cannabi.new(cannabi_params)
+
+        if cannabi.save
+            redirect_to @cannabi
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @cannabi = Cannabi.find(params[:id])
+    end
+
+    def update
+        @cannabi = Cannabi.find(params[:id])
+        if @cannabi.update_attr(cannabi_params)
+        else
+            render :edit
+        end
+    end
+
     def destroy
-        Cannabi.find(params[:id])
+        Cannabi.find(params[:id]).destroy
         redirect_to root_path
     end
 end
