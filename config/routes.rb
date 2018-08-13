d23632d28eeeb8c89fd03169abf5433820cebc15
Todd M <tboogie87@me.com>
2018-08-13 13:10:22 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 resources :cannabis
+root to: 'cannabis#index'
+root to: 'cannabis#new'
 # resources :users  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
